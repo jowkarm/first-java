193f1aa04b18975f8efd45ec395040c49df71316
jowkarm <jokar.mehdi2@gmail.com>
2024-01-05 20:33:06 -0800

    Added the header method.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        header();
         printHello();
     }
 
     private static void printHello(){
         System.out.println("Hello, world!");
+    }
+
+    private static void header(){
+        System.out.println("======================================");
     }
 }
