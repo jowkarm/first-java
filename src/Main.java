db78e88465942eeac946b8aa300e374427a0af5f
jowkarm <jokar.mehdi2@gmail.com>
2024-01-05 20:32:02 -0800

    Added the printHello method.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        printHello();
+    }
+
+    private static void printHello(){
         System.out.println("Hello, world!");
     }
 }
